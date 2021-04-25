0b39e844180079fc4ac2cf3e387ba347295424f2
Punit Narang <mepunit@gmail.com>
2021-04-25 20:05:11 +0530

    Added GraphQL Support for sensorData collection

--- index.js
+++ index.js
@@ -1,11 +1,116 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require('http');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { PubSub } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Tattva Platform!')
-})
+const MongoClient = require('mongodb').MongoClient;
+const pubsub = new PubSub();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const config = require('./config');
+let db = null;
+const connString = config.mongoDb.connectionUri;
+const collection = config.mongoDb.collection;
+
+
+async function connectDb() {
+    try {
+        const dbClient = new MongoClient(connString, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        if (!dbClient.isConnected()) await dbClient.connect();
+        return dbClient.db('stackstorm');
+    } catch (e) {
+        console.log('--->error while connecting via graphql context (db)', e)
+    }
+    return null;
+}
+
+async function startBaseServer() {
+
+  const typeDefs = gql`
+    type SensorData {
+        _id: ID!
+        Percentage_Filled: Int
+        status: String
+        deviceId: String
+        createdAt: Int
+    }
+
+    type Query {
+        sensorData: [SensorData]!
+    }
+
+    type Subscription {
+        sensorData: SensorData
+    }
+    `;
+
+    const resolvers = {
+        Query: {
+        sensorData(_parent, _args, _context, _info) {
+            return _context.db
+            .collection(collection)
+            .find().toArray()
+            .then((data) => {
+                return data
+            });
+        },
+        },
+        Subscription: {
+            sensorData: {
+              // More on pubsub below
+              subscribe: () => pubsub.asyncIterator(['NEW_DATA_RECEIVED']),
+            },
+        }
+    };
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+    db = await connectDb();
+    const changeStream = db.collection(collection).watch();
+    
+    changeStream.on('change', (next) => {
+        console.log(next.fullDocument);
+        pubsub.publish('NEW_DATA_RECEIVED', {
+            sensorData: next.fullDocument
+        });
+    });
+  
+  const server = new ApolloServer({
+    schema,
+    context: async () => {
+      if (!db) {
+        // connect if needed.
+        db = await connectDb();
+      }
+      return { db }
+    },
+    subscriptions: {
+        onConnect: (connectionParams, webSocket, context) => {
+        //   console.log('Connected!')
+        },
+        onDisconnect: (webSocket, context) => {
+        //   console.log('Disconnected!')
+        },
+      },
+  });
+
+  await server.start();
+  const PORT = 4000;
+  const app = express();
+  server.applyMiddleware({ app });
+
+  const httpServer = http.createServer(app);
+  server.installSubscriptionHandlers(httpServer);
+  await new Promise(resolve => httpServer.listen(PORT, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
+  return { server, app, httpServer };
+}
+
+startBaseServer();
